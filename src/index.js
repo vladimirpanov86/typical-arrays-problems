a00ebc0165e35b985f2345306d31d36c734acee4
Vladimir Panov <vladimirpanov86@gmail.com>
2020-10-01 10:05:10 +0700

    feat: all task complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
 exports.min = function min (array) {
+  if(array instanceof Array && array !== undefined && array.length > 0 ) return Math.min.apply(null, array);
   return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array instanceof Array && array !== undefined && array.length > 0 ) return Math.max.apply(null, array);
+  return 0
 }
 
+
 exports.avg = function avg (array) {
+  if(array instanceof Array && array !== undefined && array.length > 0 ) return array.reduce((a, b) => (a + b)) / array.length;;
   return 0;
 }
